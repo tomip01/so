5d212d9e16fcc0074f8e2b4f88f70bf82809ea1a
Tomi <tomas.paradelo@gmail.com>
2023-04-09 19:29:51 -0300

    arreglo un poco, sigue sin andar

--- taller-2/Ejercicio2/anillo_alu.c
+++ taller-2/Ejercicio2/anillo_alu.c
@@ -10,7 +10,7 @@ int generate_random_number(){
 	return (rand() % 50);
 }
 
-void ejecutar_hijo(int hijo, int start, int n, int *pipe_ida[][2], int *pipe_vuelta[][2], int *pipe_nodos[][2]){
+void ejecutar_hijo(int hijo, int start, int n, int pipe_ida[2], int pipe_vuelta[2], int pipe_nodos[][2]){
 	// cierro pipes
 	if (hijo != start) {
 		close(pipe_ida[PIPE_READ]);
@@ -28,33 +28,48 @@ void ejecutar_hijo(int hijo, int start, int n, int *pipe_ida[][2], int *pipe_vue
 		close(pipe_nodos[i][PIPE_READ]);
 	}
 
-	int empiezo;
-	read(pipe_ida[hijo][PIPE_READ], &empiezo, sizeof(empiezo));
-	if (empiezo != 0) {
-		int rand; int actual;
+	if (hijo == start) {
+		int valor_inicio;
+		read(pipe_ida[PIPE_READ], &valor_inicio, sizeof(valor_inicio));
+		int valor_secreto; int actual;
 
 		do {
-			rand = generate_random_number();
-		} while (rand >= empiezo);
-		actual = rand;		
+			valor_secreto = generate_random_number();
+		} while (valor_secreto < valor_inicio);		// genero numeros random hasta que sean mayores que el valor de inicio
+		actual = valor_inicio;
 		
-		while (actual < rand) {
-			int anterior = (hijo-1) < 0 ? n-1 : hijo-1;
+		while (actual < valor_secreto) {
+			int anterior = (hijo-1) < 0 ? n-1 : hijo-1;	// me fijo para no pasarme de rango en los extremos
 			write(pipe_nodos[hijo][PIPE_WRITE], &actual, sizeof(actual));
 			read(pipe_nodos[anterior][PIPE_READ], &actual, sizeof(actual));
 			actual++;
+			// printf("soy el hijo %i, y el actual es %i", hijo, actual);
 		}
 
-		write(pipe_vuelta[hijo][PIPE_WRITE], &actual, sizeof(actual));
+		int termino = -1;
+		write(pipe_nodos[hijo][PIPE_WRITE], &termino, sizeof(termino));
+
+		write(pipe_vuelta[PIPE_WRITE], &actual, sizeof(actual));
 		
 	} else {
 		int actual;
-		while ()
+		while (1) {
+			int anterior = (hijo-1) < 0 ? n-1 : hijo-1;
+			read(pipe_nodos[anterior][PIPE_READ], &actual, sizeof(actual));
+			if (actual == -1) break;			
+			actual++;
+			write(pipe_nodos[hijo][PIPE_WRITE], &actual, sizeof(actual));
+			printf("soy el hijo %i, y el actual es %i \n", hijo, actual);
+		}
+		
 	}
+
+	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv)
 {	
+	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
 	//Funcion para cargar nueva semilla para el numero aleatorio
 	srand(time(NULL));
 
@@ -63,7 +78,6 @@ int main(int argc, char **argv)
 	buffer = atoi(argv[2]);
 	start = atoi(argv[3]);
 
-	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
     
   	/* COMPLETAR */
     printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer, start);
@@ -83,9 +97,15 @@ int main(int argc, char **argv)
 	pid_t childrens[n];
 	for (int i = 0; i < n; ++i) {
 		pid_t hijo = fork();
-		if (hijo == 0) ejecutar_hijo(i,n,&pipe_ida,&pipe_vuelta,&pipe_nodos);
+		if (hijo == 0) ejecutar_hijo(i,start,n,pipe_ida,pipe_vuelta,pipe_nodos);
 		else childrens[i] = hijo;
 	}
+
+	// le digo que empiece al hijo start
+	write(pipe_ida[PIPE_WRITE], &buffer, sizeof(buffer));
+
+	close(pipe_ida[PIPE_READ]);
+	close(pipe_vuelta[PIPE_WRITE]);
     
 	// cierro nodos entre hijos
 	for (int i = 0; i < n; i++) {
@@ -93,14 +113,16 @@ int main(int argc, char **argv)
 		close(pipe_nodos[i][PIPE_WRITE]);
 	}
 
-	// le digo que empiece al hijo start
-	write(pipe_ida[PIPE_WRITE], &buffer, sizeof(buffer));
-	
 	// leo resultado
 	int res;
+	for (size_t i = 0; i < n; i++)
+	{
+		wait(NULL);	
+	}
+	
 	read(pipe_vuelta[PIPE_READ], &res, sizeof(res));
 
-	printf("El resultado es: %i",res);
+	printf("El resultado es: %i \n",res);
 
 	return 0;
 }
